c363b44d347800d01505cc8e1f9671fa5934824e
subin <pc666666@naver.com>
2023-07-31 16:55:22 +0900

    js

--- js/test.js
+++ js/test.js
@@ -1,10 +1,14 @@
-let userHeight = document.getElementById('height').value;
-let userWeight = document.getElementById('weight').value;
-const sum_btn = document.getElementById('sum_btn');
-let result = document.getElementById('result');
+let userHeight = document.querySelector('#height');
+let userWeight = document.querySelector('#weight');
+let sum_btn = document.querySelector('#sum_btn');
+let result = document.querySelector('#result');
 console.log(userHeight, userWeight, sum_btn, result);
 //결과보기 -> sum_btn클릭시 result에 '적정체중은 ?kg이며 ?kg초과되셨습니다.'
-let normal_w = (userHeight-100)*0.9;
+let normal_w = (userHeight.value-100)*0.9;
+console.log(normal_w);
 sum_btn.addEventListener('click',function(){
-    result.value = `적정체중은 ${normal_w}kg이며 ${userWeight-normal_w}kg 초과셨습니다.`
+    console.log(normal_w)
+    console.log(userWeight.value-normal_w)
+    console.log(typeof(userWeight.value))
+    result.value = `적정체중은 ${normal_w}kg이며 ${userWeight.value-normal_w}kg 초과셨습니다.`
 });
