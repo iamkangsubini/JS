e6017dfd4985ec8887c4a9ba754acef21269aa4d
subin <pc666666@naver.com>
2023-08-01 10:12:03 +0900

    happy

--- js/test.js
+++ js/test.js
@@ -1,14 +1,15 @@
-let userHeight = document.querySelector('#height');
-let userWeight = document.querySelector('#weight');
-let sum_btn = document.querySelector('#sum_btn');
-let result = document.querySelector('#result');
-console.log(userHeight, userWeight, sum_btn, result);
-//결과보기 -> sum_btn클릭시 result에 '적정체중은 ?kg이며 ?kg초과되셨습니다.'
-let normal_w = (userHeight.value-100)*0.9;
-console.log(normal_w);
-sum_btn.addEventListener('click',function(){
-    console.log(normal_w)
-    console.log(userWeight.value-normal_w)
-    console.log(typeof(userWeight.value))
-    result.value = `적정체중은 ${normal_w}kg이며 ${userWeight.value-normal_w}kg 초과셨습니다.`
+const userHeight = document.querySelector('#height');
+const userWeight = document.querySelector('#weight');
+const btn = document.querySelector('#sum_btn');
+const result = document.querySelector('#result');
+console.log(userHeight, userWeight, btn, result);
+btn.addEventListener('click',function(){
+    let userH = Number(userHeight.value)
+    let userW = Number(userWeight.value)
+    console.log(userH, userW)
+    let normal_W = (userH-100)*0.9
+    console.log(normal_W)
+    let goal_W = userW - normal_W
+    console.log(goal_W)
+    result.value = '적정체중은 ' + normal_W + 'kg이며 ' + goal_W +'kg 초과되셨습니다.'
 });
